04d97e4ab9e1edcbbd5d418b66470b2d493acb33
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-16 12:28:31 -0400

    Added key presses to the example

--- src/bin.rs
+++ src/bin.rs
@@ -7,25 +7,46 @@ use qs::graphics::*;
 use qs::input::*;
 use std::time::Duration;
 
+struct Entity {
+    pub bounds: Rectangle,
+    pub speed: Vector
+}
+
+impl Entity {
+    pub fn step<T: Clone>(&mut self, map: &Tilemap<T>) {
+        let (bounds, speed) = map.move_until_contact(self.bounds, self.speed);
+        self.bounds = bounds;
+        self.speed = speed;
+    }
+}
+
 struct Screen {
-    player: Rectangle,
+    player: Entity,
     map: Tilemap<i32>
 }
 
 impl State for Screen {
     fn new(_: &mut AssetManager) -> Screen {
         Screen {
-            player: Rectangle::newi(0, 0, 30, 30),
+            player: Entity {
+                bounds: Rectangle::newi(0, 0, 30, 30),
+                speed: Vector::zero()
+            },
             map: Tilemap::new(800f32, 600f32, 40f32, 40f32)
         }
     }
 
     fn tick(&mut self, draw: &mut Frontend, keys: &Keyboard, mouse: &Mouse) {
         draw.clear(WHITE);
+        self.player.speed += Vector::y() * 0.5;
+        self.player.speed.x = 0f32;
         if keys[Key::A].is_down() {
-            self.player.x -= 1f32;
+            self.player.speed.x = -2f32;
+        } else if keys[Key::D].is_down() {
+            self.player.speed.x = 2f32;
         }
-        draw.draw_rect(self.player, Transform::identity(), BLUE);
+        self.player.step(&self.map);
+        draw.draw_rect(self.player.bounds, Transform::identity(), BLUE);
         draw.present();
     }
 
