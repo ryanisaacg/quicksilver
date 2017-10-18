3ef3abc9e4f850dc6a3b1b6a6f92bebc874ccb92
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-18 10:04:29 -0400

    Added wall jumping to the example character controller

--- src/bin.rs
+++ src/bin.rs
@@ -9,10 +9,19 @@ use std::time::Duration;
 
 struct Entity {
     pub bounds: Rectangle,
-    pub speed: Vector
+    pub speed: Vector,
+    pub facing: Vector
 }
 
 impl Entity {
+    pub fn new(bounds: Rectangle) -> Entity {
+        Entity {
+            bounds: bounds,
+            speed: Vector::zero(),
+            facing: Vector::zero()
+        }
+    }
+
     pub fn step<T: Clone>(&mut self, map: &Tilemap<T>) {
         let (bounds, speed) = map.move_until_contact(self.bounds, self.speed);
         self.bounds = bounds;
@@ -28,21 +37,32 @@ struct Screen {
 impl State for Screen {
     fn new(_: &mut AssetManager) -> Screen {
         Screen {
-            player: Entity {
-                bounds: Rectangle::newi(0, 0, 30, 30),
-                speed: Vector::zero()
-            },
+            player: Entity::new(Rectangle::newi(0, 0, 32, 32)),
             map: Tilemap::new(800f32, 600f32, 40f32, 40f32)
         }
     }
 
     fn tick(&mut self, keys: &Keyboard, _: &Mouse) -> Duration {
         self.player.speed += Vector::y() * 0.5;
-        self.player.speed.x = 0f32;
+        if self.player.speed.x.abs() < 0.3 {
+            self.player.speed.x = 0.0;
+        } else {
+            self.player.speed.x *= 0.9;
+        }
         if keys[Key::A].is_down() {
-            self.player.speed.x = -2f32;
+            self.player.speed.x -= 0.4;
+            self.player.facing = -Vector::x();
         } else if keys[Key::D].is_down() {
-            self.player.speed.x = 2f32;
+            self.player.speed.x += 0.4;
+            self.player.facing = Vector::x();
+        }
+        if keys[Key::Space].is_down() {
+            if !self.map.region_empty(self.player.bounds.translate(Vector::y())) {
+                self.player.speed.y = -8f32;
+            } else if !self.map.region_empty(self.player.bounds.translate(self.player.facing)) {
+                self.player.speed.y = -8f32;
+                self.player.speed += -self.player.facing * 8;
+            }
         }
         self.player.step(&self.map);
         Duration::from_millis(10)
