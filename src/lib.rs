1a31b7b51f65d4f0634b282eda0d68c7cf5e396a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-12-09 09:33:13 -0500

    Removed unnecessary imports and trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,7 @@ pub trait State {
     fn draw(&mut self, &mut Graphics);
 }
 
-pub fn run<T: State + Send + 'static>(title: &str, width: u32, height: u32) {
-    use geom::*;
-    use graphics::*;
-
+pub fn run<T: State>(title: &str, width: u32, height: u32) {
     use glutin::GlContext;
 
     let mut events_loop = glutin::EventsLoop::new();
