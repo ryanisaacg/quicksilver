1b4d69220558b8f3bc0e6a82d23fcf789d18112e
Ryan Goldstein <ryanisaacg@gmail.com>
2018-01-09 10:05:08 -0500

    Documented the members of the Color struct

--- src/graphics/color.rs
+++ src/graphics/color.rs
@@ -1,9 +1,13 @@
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 /// An RGBA color represented by normalized floats
 pub struct Color {
+    ///The red component of the color
     pub r: f32,
+    ///The green component of the color
     pub g: f32,
+    ///The blue component of the color
     pub b: f32,
+    ///The alpha component of the color
     pub a: f32,
 }
 
