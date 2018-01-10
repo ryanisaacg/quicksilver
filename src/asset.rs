43c68be60891b53ca3041f915ab6518d9779b1e9
Ryan Goldstein <ryanisaacg@gmail.com>
2018-01-09 23:43:29 -0500

    Documented the asset module

--- src/asset.rs
+++ src/asset.rs
@@ -1,3 +1,9 @@
+//! A module that allows loading of assets
+//!
+//! Even though the desktop loads assets synchronously, the web loads them asynchronously, so this
+//! module presents a universal API for both. Additionally, the desktop may load with async at some
+//! point.
+
 use std::path::Path;
 
 #[cfg(target_arch="wasm32")]
@@ -6,27 +12,48 @@ extern "C" {
     fn is_errored(index: u32) -> bool;
 }
 
+///A trait that allows an asset to be asynchronously loaded
+///
+///Currently on desktop assets are actually loaded synchronously, but on the web they are loaded
+///async.
 pub trait Loadable: Sized + Clone {
+    ///The error type to return when an asset fails to load
     type Error: Clone;
 
+    ///Load an asset from a path
     fn load<P: AsRef<Path>>(path: P) -> LoadingAsset<Self>;
+
     #[cfg(target_arch="wasm32")]
+    ///Parse the loaded webassembly result
     fn parse_result(handle: LoadingHandle, loaded: bool, errored: bool) -> LoadingAsset<Self>;
 }
 
 #[derive(Clone)]
+///An opaque object that represents the still-loading state of an asset
 pub struct LoadingHandle(pub(crate) u32);
 
 #[derive(Clone)]
+///A wrapper for an asset that is loading, errored, or loaded
 pub enum LoadingAsset<Asset: Loadable> {
-    Loading(LoadingHandle), Loaded(Asset), Errored(Asset::Error)
+    ///An asset that is not finished loading
+    Loading(LoadingHandle), 
+    ///The asset has successfully loaded
+    Loaded(Asset),
+    ///Some error has occurred while trying to load
+    Errored(Asset::Error)
 }
 
 impl<T: Loadable> LoadingAsset<T> {
-    #[cfg(not(target_arch="wasm32"))]
-    pub fn update(&mut self) {}
-    #[cfg(target_arch="wasm32")]
+    ///Update the loading asset possibly produced a loaded state or an error
     pub fn update(&mut self) {
+        self.update_impl();
+    }
+
+    #[cfg(not(target_arch="wasm32"))]
+    fn update_impl(&mut self) {}
+
+    #[cfg(target_arch="wasm32")]
+    fn update_impl(&mut self) {
         *self = match self.clone() {
             LoadingAsset::Loading(handle) =>  {
                 let loaded = unsafe { is_loaded(handle.0) };
