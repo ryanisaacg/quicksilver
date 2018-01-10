9e3a3fd802f09a6afe2d8951b3c1fdc41f4229df
Ryan Goldstein <ryanisaacg@gmail.com>
2018-01-10 00:05:21 -0500

    Documented the geometry module

--- src/geom/mod.rs
+++ src/geom/mod.rs
@@ -1,3 +1,12 @@
+//! A 2D geometry module
+//!
+//! It contains basic shapes such as rectangles and circles, as well as vectors, lines, and a
+//! universal Shape API. It also has matrix-backed Transforms for arbitrary constant-time 2D
+//! transformations, such as rotating, scaling, or translating. 
+//!
+//! The Tilemap allows 2D storage of data in a world-like grid, and also moving objects at given
+//! speeds around the map, which is highly useful for games like platformers.
+
 mod vector;
 mod line;
 mod rect;
