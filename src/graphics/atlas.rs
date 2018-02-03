afdb63aa9ce2fa0a746391fa032ccd6f606298b0
Ryan Goldstein <ryanisaacg@gmail.com>
2018-02-03 17:30:52 -0500

    Finished the conversion of the Atlas into Futures

--- src/graphics/atlas.rs
+++ src/graphics/atlas.rs
@@ -1,20 +1,19 @@
 extern crate futures;
 
 use futures::{Async, Future, Poll};
-use futures::future::{AndThen, JoinAll, Map};
+use futures::future::{JoinAll, join_all, Map, Then};
 use geom::{Rectangle, Vector};
-use graphics::{Image, ImageError};
+use graphics::{Image, ImageLoader, ImageError};
 use util::FileLoader;
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::Debug;
-use std::iter::Map;
 use std::num::ParseIntError;
 use std::path::Path;
 use std::str::{FromStr, ParseBoolError, Split};
 
 #[derive(Clone)]
-pub struct Region {
+struct Region {
     name: String,
     rotate: bool,
     region: Rectangle,
@@ -31,19 +30,50 @@ pub struct Atlas {
 }
 
 impl Atlas {
+    /// Load an atlas at a given path
     pub fn load<P: AsRef<Path>>(path: P) -> AtlasLoader {
-        FileLoader::new(path)
-            .map(&parse)
-            .and_then(&create)
+        AtlasLoader(FileLoader::load(path)
+            .then(parse as fn(Result<String, ()>) -> ManifestLoader)
+            .map(create as fn((Vec<Image>, Vec<Vec<Region>>)) -> Atlas))
     }
 }
 
+type ManifestContents = Result<(JoinAll<Vec<ImageLoader>>, Vec<Vec<Region>>), AtlasError>;
+struct ManifestLoader(ManifestContents);
 
-type Intermediate = (JoinAll<Image>, Vec<Vec<Region>>);
-pub type AtlasLoader = Map<Map<FileLoader, fn(String) -> (JoinAll<Image>, Vec<Vec<Region>>)>,
-                               fn((Vec<Image>, Vec<Vec<Region>>)) -> Atlas>;
+type Parser = Then<FileLoader, ManifestLoader, fn(Result<String, ()>) -> ManifestLoader>;
+type Creator = Map<Parser, fn((Vec<Image>, Vec<Vec<Region>>)) -> Atlas>;
+
+/// The Future used to load an Atlas object
+pub struct AtlasLoader(Creator);
+
+impl Future for ManifestLoader {
+    type Item = (Vec<Image>, Vec<Vec<Region>>);
+    type Error = AtlasError;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        match self.0 {
+            Ok(ref mut item) => match item.0.poll() {
+                Ok(Async::Ready(images)) => Ok(Async::Ready((images, item.1.clone()))),
+                Ok(Async::NotReady) => Ok(Async::NotReady),
+                Err(err) => Err(err.into())
+            }
+            Err(ref err) => Err(err.clone())
+        }
+    }
+}
+
+impl Future for AtlasLoader {
+    type Item = Atlas;
+    type Error = AtlasError;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.0.poll()
+    }
+}
 
 fn create(data: (Vec<Image>, Vec<Vec<Region>>)) -> Atlas {
+    let (images, regions) = data;
     let mut images = images.into_iter()
         //Match each image with its list of regions
         .zip(regions.into_iter())
@@ -83,8 +113,9 @@ fn create(data: (Vec<Image>, Vec<Vec<Region>>)) -> Atlas {
     Atlas { data }
 }
 
-fn parse(data: &str) -> Result<LoadingAsset<Atlas>, AtlasError> {
-    fn get_values_from_line<'a, T: FromStr>(line: &'a str) -> Map<Split<'a, &'static str>, fn(&'a str) -> T> 
+fn parse(data: Result<String, ()>) -> ManifestLoader {
+    use std::iter::Map as IterMap;
+    fn get_values_from_line<'a, T: FromStr>(line: &'a str) -> IterMap<Split<'a, &'static str>, fn(&'a str) -> T> 
         where <T as FromStr>::Err: Debug {
         fn parse<'a, T: FromStr>(item: &'a str) -> T where <T as FromStr>::Err: Debug { item.parse().unwrap() }
         let mut split = line.split(": ");
@@ -98,40 +129,46 @@ fn parse(data: &str) -> Result<LoadingAsset<Atlas>, AtlasError> {
             None => Err(AtlasError::ParseError("Unexpected end of data"))?
         }
     }
-    let mut lines = data.lines();
-    let mut images = Vec::new();
-    let mut regions = Vec::new();
-    while let Some(line) = lines.next() {
-        //TODO: make relative to atlas location
-        images.push(Image::load(line));
-        regions.push(Vec::new());
-        //Skip some lines the loader doesn't use
-        for _ in 0..4 {
-            getval(&mut lines)?;
-        }
+    fn parse_body(data: String) -> ManifestContents {
+        let mut lines = data.lines();
+        let mut images = Vec::new();
+        let mut regions = Vec::new();
         while let Some(line) = lines.next() {
-            //If there's an empty line, move onto a different page
-            if line.len() == 0 { break; }
-            let name = line.to_owned();
-            let mut rotate = get_values_from_line(getval(&mut lines)?);
-            let mut xy = get_values_from_line::<i32>(getval(&mut lines)?);
-            let mut size = get_values_from_line(getval(&mut lines)?);
-            //Skip more &mut lines the loader doesn't use
-            for _ in 0..2 {
+            //TODO: make relative to atlas location
+            images.push(Image::load(line));
+            regions.push(Vec::new());
+            //Skip some lines the loader doesn't use
+            for _ in 0..4 {
                 getval(&mut lines)?;
             }
-            let mut orig = get_values_from_line::<i32>(getval(&mut lines)?);
-            let mut offset = get_values_from_line::<i32>(getval(&mut lines)?);
-            let index = getval(&mut get_values_from_line(getval(&mut lines)?))?;
-            let rotate = getval(&mut rotate)?;
-            let region = Rectangle::new(getval(&mut xy)?, getval(&mut xy)?, getval(&mut size)?, getval(&mut size)?);
-            let original_size = Vector::new(getval(&mut orig)?, getval(&mut orig)?);
-            let offset = Vector::new(getval(&mut offset)?, getval(&mut offset)?);
-            let center = region.center() + (original_size - region.size() - offset.x_comp() + offset.y_comp());
-            regions.last_mut().unwrap().push(Region { name, region, rotate, center, index });
+            while let Some(line) = lines.next() {
+                //If there's an empty line, move onto a different page
+                if line.len() == 0 { break; }
+                let name = line.to_owned();
+                let mut rotate = get_values_from_line(getval(&mut lines)?);
+                let mut xy = get_values_from_line::<i32>(getval(&mut lines)?);
+                let mut size = get_values_from_line(getval(&mut lines)?);
+                //Skip more &mut lines the loader doesn't use
+                for _ in 0..2 {
+                    getval(&mut lines)?;
+                }
+                let mut orig = get_values_from_line::<i32>(getval(&mut lines)?);
+                let mut offset = get_values_from_line::<i32>(getval(&mut lines)?);
+                let index = getval(&mut get_values_from_line(getval(&mut lines)?))?;
+                let rotate = getval(&mut rotate)?;
+                let region = Rectangle::new(getval(&mut xy)?, getval(&mut xy)?, getval(&mut size)?, getval(&mut size)?);
+                let original_size = Vector::new(getval(&mut orig)?, getval(&mut orig)?);
+                let offset = Vector::new(getval(&mut offset)?, getval(&mut offset)?);
+                let center = region.center() + (original_size - region.size() - offset.x_comp() + offset.y_comp());
+                regions.last_mut().unwrap().push(Region { name, region, rotate, center, index });
+            }
         }
+        Ok((join_all(images), regions))
     }
-    Ok(LoadingAsset::Loading((LoadingAsset::Loading(images), regions)))
+    match data {
+        Ok(data) => ManifestLoader(parse_body(data)),
+        Err(err) => ManifestLoader(Err(err.into()))
+    }
 }
 
 
@@ -148,6 +185,12 @@ pub enum AtlasError {
     IoError
 }
 
+impl From<ImageError> for AtlasError {
+    fn from(err: ImageError) -> AtlasError {
+        AtlasError::ImageError(err)
+    }
+}
+
 impl From<ParseIntError> for AtlasError {
     fn from(_: ParseIntError) -> AtlasError {
         AtlasError::ParseError("Failed to parse an int")
@@ -160,3 +203,8 @@ impl From<ParseBoolError> for AtlasError {
     }
 }
 
+impl From<()> for AtlasError {
+    fn from(_: ()) -> AtlasError {
+        AtlasError::IoError
+    }
+}
--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,14 @@
+//! A collection of general utilities
+
 extern crate futures;
 
 use futures::{Async, Future, Poll};
 use std::path::Path;
 
+/// A Future that loads a file into an owned string
+///
+/// It exists for loading files from the server with Javascript on the web, and providing a unified
+/// API between desktop and the web when it comes to file loading
 pub struct FileLoader {
     #[cfg(not(target_arch="wasm32"))]
     data: Result<String, ()>,
@@ -11,7 +17,8 @@ pub struct FileLoader {
 }
 
 impl FileLoader {
-    pub fn new<P: AsRef<Path>>(path: P) -> FileLoader {
+    /// Create a FileLoader for a given path
+    pub fn load<P: AsRef<Path>>(path: P) -> FileLoader {
         FileLoader::new_impl(path)
     }
 
@@ -19,8 +26,8 @@ impl FileLoader {
     fn new_impl<P: AsRef<Path>>(path: P) -> FileLoader {
         use std::fs::File;
         use std::io::Read;
-        let data = String::new();
-        if let Ok(file) = File::open(path) {
+        let mut data = String::new();
+        if let Ok(ref mut file) = File::open(path) {
             if let Ok(_) = file.read_to_string(&mut data) {
                 FileLoader { data: Ok(data) }
             } else {
@@ -38,7 +45,7 @@ impl FileLoader {
         extern "C" {
             fn load_text_file(path: *mut c_char) -> u32;
         }
-        ImageLoader {
+        FileLoader {
             id: unsafe { load_text_file(CString::new(path.as_ref().to_str().unwrap()).unwrap().into_raw()) }
         }
     }
@@ -61,12 +68,12 @@ impl Future for FileLoader {
         use std::ffi::CString;
         use std::os::raw::c_char;
         extern "C" {
-            fn is_text_file_loaded(id: u32);
-            fn is_text_file_errored(id: u32);
+            fn is_text_file_loaded(id: u32) -> bool;
+            fn is_text_file_errored(id: u32) -> bool;
             fn text_file_contents(id: u32) -> *mut c_char;
         }
-        if is_text_file_loaded(self.id) {
-            if is_text_file_errored(self.id) {
+        if unsafe { is_text_file_loaded(self.id) } {
+            if unsafe { is_text_file_errored(self.id) } {
                 Err(())
             } else {
                 Ok(Async::Ready(unsafe { 
