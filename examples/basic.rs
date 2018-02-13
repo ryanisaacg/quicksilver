f97ba85b63f40e8509ff32ff5a3f80d96c10f526
Ryan Goldstein <ryanisaacg@gmail.com>
2018-02-13 15:40:26 -0500

    Added the most basic example possible

---
+++ examples/basic.rs
@@ -0,0 +1,23 @@
+// The most basic example- it should just open a black window and set the window title to Hello
+// world!
+#[macro_use]
+extern crate quicksilver;
+
+use quicksilver::State;
+use quicksilver::graphics::{Canvas, Window, WindowBuilder};
+
+// An empty structure because we don't need to store any state
+struct BlackScreen;
+
+impl State for BlackScreen {
+    fn configure() -> (Window, Canvas) {
+        // Create a Window with the title "Hello world!" that is 800 x 600 pixels
+        WindowBuilder::new()
+            .build("Hello world!", 800, 600)
+    }
+
+   fn new() -> BlackScreen { BlackScreen }
+}
+
+//Run the application on both desktop and web
+run!(BlackScreen);
