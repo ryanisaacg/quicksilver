2976e2502835d83608e3faec8ea5f8946713d0c5
Ryan Goldstein <ryanisaacg@gmail.com>
2018-03-04 21:02:03 -0500

    Added a function to report all collisions of the objects in the quadtree

--- src/geom/quadtree.rs
+++ src/geom/quadtree.rs
@@ -1,12 +1,12 @@
 use geom::{Bounded, Rectangle, Shape};
 
 /// A QuadTree that stores bounded objects and allows for collision checking
-pub struct QuadTree<T: Bounded> {
+pub struct QuadTree<T: Bounded + Clone> {
     areas: Vec<Rectangle>,
     nodes: Vec<Vec<T>>
 }
 
-impl<T: Bounded> QuadTree<T> {
+impl<T: Bounded + Clone> QuadTree<T> {
     /// Create a quadtree covering a given area with a given 'depth'
     ///
     /// Depth is more of an implementation detail: it is the number of levels in the tree,
@@ -56,33 +56,58 @@ impl<T: Bounded> QuadTree<T> {
         self.nodes[index as usize].push(object);
     }
 
-    fn recurse_up<F>(&mut self, index: i32, bounds: Shape, f: &mut F) where F: FnMut(&mut T) {
+    /// Apply a function to all items that intersect with the shape
+    pub fn query<F>(&mut self, bounds: Shape, mut f: F) where F: FnMut(&mut T) { 
+        let index = self.get_index(bounds.bounding_box());
+        self.query_recurse_up(index, bounds, &mut f);
+        self.query_recurse_down(index, bounds, &mut f);
+    }
+
+    fn query_recurse_up<F>(&mut self, index: i32, bounds: Shape, f: &mut F) where F: FnMut(&mut T) {
         for object in self.nodes[index as usize].iter_mut().filter(|item| item.overlaps(&bounds)) {
             f(object);
         }
         if index > 0 {
-            self.recurse_up((index - 1) / 4, bounds, f);
+            self.query_recurse_up((index - 1) / 4, bounds, f);
         }
     }
 
-    fn recurse_down<F>(&mut self, index: i32, bounds: Shape, f: &mut F) where F: FnMut(&mut T) {
+    fn query_recurse_down<F>(&mut self, index: i32, bounds: Shape, f: &mut F) where F: FnMut(&mut T) {
         if index * 4 + 5 < self.areas.len() as i32 {
             for object in self.nodes[index as usize].iter_mut().filter(|item| item.overlaps(&bounds)) {
                 f(object);
             }
-            for i in 0..4 {
+            for i in 1..5 {
                 let child = index * 4 + i;
-                self.recurse_down(child, bounds, f);
+                self.query_recurse_down(child, bounds, f);
             }
         }
     }
 
-    /// Apply a function to all items that intersect with the shape
-    pub fn query<F>(&mut self, bounds: Shape, mut f: F) where F: FnMut(&mut T) {
-        
-        let index = self.get_index(bounds.bounding_box());
-        self.recurse_up(index, bounds, &mut f);
-        self.recurse_down(index, bounds, &mut f);
+    /// Check all objects in the tree for collision
+    ///
+    /// If any collisions occur, one object is mutably borrowed and the other is cloned and they
+    /// are passed to the provided functions
+    pub fn check<F>(&mut self, mut collision_action: F) where F: FnMut(&mut T, T) {
+        for i in 0..self.nodes.len() {
+            self.check_recurse(i, i, &mut collision_action);
+        }
+    }
+
+    fn check_recurse<F>(&mut self, a_list: usize, b_list: usize, f: &mut F) where F: FnMut(&mut T, T) {
+        if b_list < self.nodes.len() {
+            for i in 0..self.nodes[a_list].len() {
+                for j in 0..self.nodes[b_list].len() {
+                    if (a_list != b_list || i != j) && self.nodes[a_list][i].overlaps(&self.nodes[b_list][j]) {
+                        let node = self.nodes[b_list][j].clone();
+                        f(&mut self.nodes[a_list][i], node);
+                    }
+                }
+            }
+            for i in 1..5 {
+                self.check_recurse(a_list, b_list * 4 + i, f);
+            }
+        }
     }
 }
 
@@ -98,5 +123,21 @@ mod tests {
         let mut found = false;
         quadtree.query(Shape::Vect(Vector::new(35, 35)), |_| found = true);
         assert!(found);
-    }    
+    }
+
+    #[test]
+    fn collision() {
+        let mut quadtree = QuadTree::new(Rectangle::new_sized(100, 100), 10);
+        quadtree.insert(Rectangle::new(20, 20, 50, 50));
+        quadtree.insert(Rectangle::new(30, 30, 50, 50));
+        quadtree.insert(Rectangle::new(100, 30, 50, 50));
+        let mut collisions = [[false, false, false], [false, false, false], [false, false, false]];
+        quadtree.check(|a, b| {
+            let a_index = if a.x == 20.0 { 0 } else if a.x == 30.0 { 1 } else { 2 };
+            let b_index = if b.x == 20.0 { 0 } else if b.x == 30.0 { 1 } else { 2 };
+            collisions[a_index][b_index] = true;
+        });
+        let expected_collisions = [[false, true, false], [true, false, false], [false, false, false]];
+        assert_eq!(collisions, expected_collisions);
+    }
 }
