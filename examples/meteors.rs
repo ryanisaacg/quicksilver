16e07f0c28056bc83bd44d2f416cecd7b77c34b3
Ryan Goldstein <ryanisaacg@gmail.com>
2018-04-27 18:43:15 -0400

    Added meteors that move around the play area

--- examples/meteors.rs
+++ examples/meteors.rs
@@ -1,6 +1,7 @@
 // A tribute to a certain Atari arcade game
 extern crate futures;
 extern crate quicksilver;
+extern crate rand;
 
 use futures::{
     Future, Async
@@ -8,9 +9,10 @@ use futures::{
 use quicksilver::{
     State, run,
     geom::{Circle, Positioned, Rectangle, Transform, Vector},
-    graphics::{Color, Sprite, Image, ImageLoader, Window, WindowBuilder, View},
+    graphics::{Draw, Color, Image, ImageLoader, Window, WindowBuilder, View},
     input::Key
 };
+use rand::Rng;
 
 enum Meteors {
     Loading(ImageLoader),
@@ -62,7 +64,7 @@ impl Entity {
     }
 
     fn draw(&self, image: &Image, window: &mut Window) {
-        window.draw(&Sprite::image(image, self.bounds.center())
+        window.draw(&Draw::image(image, self.bounds.center())
             .with_transform(Transform::rotate(self.facing)));
     }
 }
@@ -70,7 +72,8 @@ impl Entity {
 struct GameState {
     player: Entity,
     camera: Rectangle,
-    player_image: Image
+    player_image: Image,
+    meteors: Vec<Entity>
 }
 
 impl GameState {
@@ -78,11 +81,27 @@ impl GameState {
         GameState {
             player: Entity::new(Circle::newv(Vector::zero(), 16)),
             camera: Rectangle::new_sized(SCREEN_WIDTH, SCREEN_HEIGHT),
-            player_image
+            player_image,
+            meteors: Vec::new()
         }
     }
 
     fn update(&mut self, window: &mut Window) {
+        let mut rng = rand::thread_rng();
+        for meteor in self.meteors.iter_mut() {
+            meteor.facing += meteor.velocity.x + meteor.velocity.y;
+            meteor.tick();
+        }
+        let player_center = self.player.bounds.center();
+        self.meteors.retain(|meteor| (meteor.bounds.center() - player_center).len() < 1500.0);
+        while self.meteors.len() < 20 {
+            let diff_x = rng.gen_range(450, 650) * rng.choose(&[-1, 1]).unwrap();
+            let diff_y = rng.gen_range(350, 550) * rng.choose(&[-1, 1]).unwrap();
+            let center = self.player.bounds.center()  + Vector::new(diff_x, diff_y);
+            let mut entity = Entity::new(Circle::newv(center, 20));
+            entity.velocity = Vector::new(rng.gen_range(-5, 5), rng.gen_range(-5, 5));
+            self.meteors.push(entity);
+        }
         if window.keyboard()[Key::A].is_down() {
             self.player.facing -= PLAYER_ROTATION;
         }
@@ -103,6 +122,9 @@ impl GameState {
     fn draw(&mut self, window: &mut Window) {
         window.clear(Color::black());
         self.player.draw(&self.player_image, window);
+        for meteor in self.meteors.iter() {
+            window.draw(&Draw::circle(meteor.bounds).with_color(Color { r: 0.5, g: 0.5, b: 0.0, a: 1.0 }));
+        }
         window.present();
     }
 }
