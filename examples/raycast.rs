7f9bfc3c8109ef281fa75c8d78bc956eac80a8ec
Ryan Goldstein <ryanisaacg@gmail.com>
2018-05-08 09:04:21 -0400

    Fully implemented the lighting effect

--- examples/raycast.rs
+++ examples/raycast.rs
@@ -6,7 +6,7 @@ use quicksilver::{
     State, run,
     geom::{Rectangle, Vector},
     input::Event,
-    graphics::{Color, GpuTriangle, WindowBuilder, Window, Vertex}
+    graphics::{Color, Draw, GpuTriangle, WindowBuilder, Window, Vertex}
 };
 use nalgebra::{Isometry2, zero};
 use ncollide::{
@@ -30,7 +30,7 @@ impl State for Raycast {
     fn new() -> Raycast {
         let regions = vec![
             Rectangle::new_sized(800, 600),
-            Rectangle::new(200, 200, 400, 400)
+            Rectangle::new(200, 200, 300, 300)
         ];
         let targets = regions.iter().flat_map(|region| {
             vec![region.top_left(), 
@@ -47,39 +47,45 @@ impl State for Raycast {
 
     fn event(&mut self, event: &Event, _: &mut Window) {
         if let &Event::MouseMoved(mouse) = event {
+            self.vertices.clear();
             let distance_to = |point: &Vector| (*point - mouse).len();
             let angle_to = |point: &Vertex| (point.pos - mouse).angle();
-            self.vertices.clear();
             // Raycast towards all targets and find the vertices
             for i in 0..self.targets.len() {
-                // Create a Ray from the mouse to the target
-                let start = mouse.into_point();
-                let direction = (self.targets[i] - mouse).normalize().into_vector();
-                let ray = Ray::new(start, direction);
-                // Perform the actual raycast, returning the target and an iterator of collisions
-                let identity = Isometry2::new(zero(), zero());
-                let pos = self.regions
-                    .iter()
-                    .filter_map(|region| region
-                        .into_aabb()
-                        .toi_with_ray(&identity, &ray, false))
-                    .map(|toi: f32| (ray.origin + toi * ray.dir).into())
-                    .min_by(|a: &Vector, b: &Vector| distance_to(a)
-                        .partial_cmp(&distance_to(b))
-                        .unwrap_or(Ordering::Equal))
-                    .unwrap_or(self.targets[i]);
-                self.vertices.push(Vertex {
-                    pos,
-                    tex_pos: None,
-                    col: Color::white()
-                });
+                let angle = (self.targets[i] - mouse).angle();
+                let mut cast_ray = |direction: f32| {
+                    // Create a Ray from the mouse to the target
+                    let start = mouse.into_point();
+                    let direction = Vector::from_angle(direction).into_vector();
+                    let ray = Ray::new(start, direction);
+                    // Perform the actual raycast, returning the target and an iterator of collisions
+                    let identity = Isometry2::new(zero(), zero());
+                    let pos = self.regions
+                        .iter()
+                        .filter_map(|region| region
+                            .into_aabb()
+                            .toi_with_ray(&identity, &ray, false))
+                        .map(|toi: f32| (ray.origin + toi * ray.dir).into())
+                        .min_by(|a: &Vector, b: &Vector| distance_to(a)
+                            .partial_cmp(&distance_to(b))
+                            .unwrap_or(Ordering::Equal))
+                        .unwrap();
+                    self.vertices.push(Vertex {
+                        pos,
+                        tex_pos: None,
+                        col: Color::white()
+                    });
+                };
+                cast_ray(angle - 0.001);
+                cast_ray(angle);
+                cast_ray(angle + 0.001);
             }
             // Sort the vertices to form a visibility polygon
             self.vertices.sort_by(|a, b| angle_to(a)
                 .partial_cmp(&angle_to(b))
                 .unwrap_or(Ordering::Equal));
             //Insert the mouse as a vertex
-            self.vertices.push(Vertex {
+            self.vertices.insert(0, Vertex {
                 pos: mouse,
                 tex_pos: None,
                 col: Color::white()
@@ -89,14 +95,19 @@ impl State for Raycast {
 
     fn draw(&mut self, window: &mut Window) {
         window.clear(Color::black());
-        if self.vertices.len() >= 3 {
-            let indices = repeat(0.0).take(self.vertices.len() - 1).enumerate().map(|(index, z)| GpuTriangle {
+        let triangle_count = self.vertices.len() as u32 - 1;
+        let indices = repeat(0.0)
+            .take(triangle_count as usize)
+            .enumerate()
+            .map(|(index, z)| GpuTriangle {
                 z,
-                indices: [0, index as u32, index as u32 + 1],
+                indices: [0, 
+                    index as u32 + 1,
+                    (index as u32 + 1) % triangle_count + 1],
                 image: None
             });
-            window.add_vertices(self.vertices.iter().cloned(), indices);
-        }
+        window.add_vertices(self.vertices.iter().cloned(), indices);
+        window.draw(&Draw::rectangle(Rectangle::new(200, 200, 300, 300)).with_color(Color::blue()));
         window.present();
     }
 }
