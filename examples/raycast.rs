52550e76f78ae67c1c25c4ee3fd627dc867d30bf
Ryan Goldstein <ryanisaacg@gmail.com>
2018-05-20 18:14:10 -0400

    Abandoned using surfaces and fixed the panic-on-resize issue

--- examples/raycast.rs
+++ examples/raycast.rs
@@ -6,7 +6,7 @@ use quicksilver::{
     State, run,
     geom::{Rectangle, Vector},
     input::Event,
-    graphics::{BlendMode, Color, Draw, GpuTriangle, Surface, WindowBuilder, Window, Vertex}
+    graphics::{Color, Draw, GpuTriangle, WindowBuilder, Window, Vertex}
 };
 use nalgebra::{Isometry2, zero};
 use ncollide2d::{
@@ -24,79 +24,8 @@ struct Raycast {
     targets: Vec<Vector>,
     // the vertices to draw to the screen
     vertices: Vec<Vertex>,
-    // a temporary buffer to write to
-    sketch: Surface,
-    // the sum total of lighting effects
-    lightmap: Surface
 }
 
-impl Raycast {
-    fn lightsource(&mut self, source: Vector, window: &mut Window) {
-        self.vertices.clear();
-        let distance_to = |point: &Vector| (*point - source).len();
-        let angle_to = |point: &Vertex| (point.pos - source).angle();
-        // Raycast towards all targets and find the vertices
-        for i in 0..self.targets.len() {
-            let angle = (self.targets[i] - source).angle();
-            let mut cast_ray = |direction: f32| {
-                // Create a Ray from the source to the target
-                let start = source.into_point();
-                let direction = Vector::from_angle(direction).into_vector();
-                let ray = Ray::new(start, direction);
-                // Perform the actual raycast, returning the target and an iterator of collisions
-                let identity = Isometry2::new(zero(), zero());
-                let pos = self.regions
-                    .iter()
-                    .filter_map(|region| region
-                        .into_aabb()
-                        .toi_with_ray(&identity, &ray, false))
-                    .map(|toi: f32| (ray.origin + toi * ray.dir).into())
-                    .min_by(|a: &Vector, b: &Vector| distance_to(a)
-                        .partial_cmp(&distance_to(b))
-                        .unwrap_or(Ordering::Equal))
-                    .unwrap();
-                self.vertices.push(Vertex {
-                    pos,
-                    tex_pos: None,
-                    col: Color { r: 0.2, g: 0.2, b: 0.2, a: 1.0 }
-                });
-            };
-            cast_ray(angle - 0.001);
-            cast_ray(angle);
-            cast_ray(angle + 0.001);
-        }
-        // Sort the vertices to form a visibility polygon
-        self.vertices.sort_by(|a, b| angle_to(a)
-            .partial_cmp(&angle_to(b))
-            .unwrap_or(Ordering::Equal));
-        //Insert the source as a vertex
-        self.vertices.insert(0, Vertex {
-            pos: source,
-            tex_pos: None,
-            col: Color { r: 0.2, g: 0.2, b: 0.2, a: 1.0 }
-        });
-        self.sketch.render_to(window, |window| {
-            window.reset_blend_mode();
-            window.clear(Color::black());
-            let triangle_count = self.vertices.len() as u32 - 1;
-            let indices = repeat(0.0)
-                .take(triangle_count as usize)
-                .enumerate()
-                .map(|(index, z)| GpuTriangle {
-                    z,
-                    indices: [0, 
-                        index as u32 + 1,
-                        (index as u32 + 1) % triangle_count + 1],
-                    image: None
-                });
-            window.add_vertices(self.vertices.iter().cloned(), indices);
-        });
-        self.lightmap.render_to(window, |window| {
-            window.set_blend_mode(BlendMode::Additive);
-            window.draw(&Draw::image(&self.sketch.image(), Vector::new(400, 300)));
-        });
-    }
-}
 
 impl State for Raycast {
     fn new() -> Raycast {
@@ -118,27 +47,76 @@ impl State for Raycast {
         Raycast {
             regions,
             targets,
-            lightmap: Surface::new(800, 600),
-            sketch: Surface::new(800, 600),
-            vertices: Vec::new()
+            vertices: Vec::new(),
         }
     }
 
     fn event(&mut self, event: &Event, window: &mut Window) {
-        if let &Event::MouseMoved(mouse) = event {
-            self.lightmap.render_to(window, |window| window.clear(Color::black()));
-            for i in 0..6 {
-                let angle = i * 360 / 6;
-                let offset = Vector::from_angle(angle) * 8;
-                let source = mouse + offset;
-                self.lightsource(source.clamp(Vector::zero(), Vector::new(800, 600)), window);
+        if let &Event::MouseMoved(_) = event {
+            let mouse = window.mouse().pos();
+            self.vertices.clear();
+            let distance_to = |point: &Vector| (*point - mouse).len();
+            let angle_to = |point: &Vertex| (point.pos - mouse).angle();
+            // Raycast towards all targets and find the vertices
+            for i in 0..self.targets.len() {
+                let angle = (self.targets[i] - mouse).angle();
+                let mut cast_ray = |direction: f32| {
+                    // Create a Ray from the mouse to the target
+                    let start = mouse.into_point();
+                    let direction = Vector::from_angle(direction).into_vector();
+                    let ray = Ray::new(start, direction);
+                    // Perform the actual raycast, returning the target and an iterator of collisions
+                    let identity = Isometry2::new(zero(), zero());
+                    let cast = self.regions
+                        .iter()
+                        .filter_map(|region| region
+                            .into_aabb()
+                            .toi_with_ray(&identity, &ray, false))
+                        .map(|toi: f32| (ray.origin + toi * ray.dir).into())
+                        .min_by(|a: &Vector, b: &Vector| distance_to(a)
+                            .partial_cmp(&distance_to(b))
+                            .unwrap_or(Ordering::Equal));
+                    if let Some(pos) = cast {
+                        self.vertices.push(Vertex {
+                            pos,
+                            tex_pos: None,
+                            col: Color::white()
+                        });
+                    }
+                };
+                cast_ray(angle - 0.001);
+                cast_ray(angle);
+                cast_ray(angle + 0.001);
             }
+            // Sort the vertices to form a visibility polygon
+            self.vertices.sort_by(|a, b| angle_to(a)
+                .partial_cmp(&angle_to(b))
+                .unwrap_or(Ordering::Equal));
+            //Insert the mouse as a vertex
+            self.vertices.insert(0, Vertex {
+                pos: mouse,
+                tex_pos: None,
+                col: Color::white()
+            });
         }
     }
 
     fn draw(&mut self, window: &mut Window) {
         window.clear(Color::black());
-        window.draw(&Draw::image(self.sketch.image(), Vector::new(400, 300)));
+        if self.vertices.len() >= 3 {
+            let triangle_count = self.vertices.len() as u32 - 1;
+            let indices = repeat(0.0)
+                .take(triangle_count as usize)
+                .enumerate()
+                .map(|(index, z)| GpuTriangle {
+                    z,
+                    indices: [0, 
+                        index as u32 + 1,
+                        (index as u32 + 1) % triangle_count + 1],
+                    image: None
+                });
+            window.add_vertices(self.vertices.iter().cloned(), indices);
+        }
         window.present();
     }
 }
