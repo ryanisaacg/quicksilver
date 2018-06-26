2543b00211d927ece05d1d656b68f2bc495d99b2
Ryan G <ryanisaacg@users.noreply.github.com>
2018-06-26 07:47:47 -0400

    Add Asset to help manage loading futures (#261)
    It automates away polling and updating an enum state, and also obviates
    the need to actually store the Futures involved. It even eliminates the
    need to name or understand the futures, as the Asset manages that for
    you.

--- examples/image.rs
+++ examples/image.rs
@@ -2,43 +2,32 @@
 extern crate quicksilver;
 
 use quicksilver::{
-    Async, Future, Result, State, run,
+    Asset, Result, State, run,
     geom::Vector,
-    graphics::{Color, Image, ImageLoader, Sprite, Window, WindowBuilder}
+    graphics::{Color, Image, Sprite, Window, WindowBuilder}
 };
 
-enum ImageViewer {
-    Loading(ImageLoader),
-    Loaded(Image)
+struct ImageViewer {
+    asset: Asset<Image>
 }
 
 impl State for ImageViewer {
     fn new() -> Result<ImageViewer> { 
-        Ok(ImageViewer::Loading(Image::load("examples/assets/image.png")))
+        let asset = Asset::new(Image::load("examples/assets/image.png"));
+        Ok(ImageViewer {
+            asset
+        })
     }
 
-   fn update(&mut self, _: &mut Window) -> Result<()> {
-       // Check to see the progress of the loading image
-       let result = match self {
-           &mut ImageViewer::Loading(ref mut loader) => loader.poll().unwrap(),
-           _ => Async::NotReady
-       };
-       // If the image has been loaded move to the loaded state
-       if let Async::Ready(asset) = result {
-           *self = ImageViewer::Loaded(asset);
-       }
-       Ok(())
-   }
-
-   fn draw(&mut self, window: &mut Window) -> Result<()> {
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::white());
-        // If the image is loaded draw it
-        if let &mut ImageViewer::Loaded(ref image) = self {
+        self.asset.execute(|image| {
             window.draw(&Sprite::image(image, Vector::new(400, 300)));
-        }
+            Ok(())
+        })?;
         window.present();
         Ok(())
-   }
+    }
 }
 
 fn main() {
