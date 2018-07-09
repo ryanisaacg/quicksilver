bfb0b5928b204271631921a76b3bbe2c396e3c40
Ryan Goldstein <ryanisaacg@gmail.com>
2018-07-08 20:38:35 -0400

    Add an example of drawing an RGB triangle using vertices and indices

---
+++ examples/rgb-triangle.rs
@@ -0,0 +1,32 @@
+// Draw the classic triangle to the screen
+extern crate quicksilver;
+
+use quicksilver::{
+    run, Result, State,
+    geom::{Circle, Rectangle, Transform, Vector},
+    graphics::{Color, GpuTriangle, Vertex, Window, WindowBuilder}
+};
+
+struct RgbTriangle;
+
+impl State for RgbTriangle {
+    fn new() -> Result<RgbTriangle> {
+        Ok(RgbTriangle)
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::BLACK)?;
+        let vertices = [
+            Vertex::new_untextured(Vector::new(400, 200), Color::RED),
+            Vertex::new_untextured(Vector::new(200, 400), Color::BLUE),
+            Vertex::new_untextured(Vector::new(600, 400), Color::GREEN),
+        ];
+        let indices = [ GpuTriangle::new_untextured([0, 1, 2], 0.0) ];
+        window.add_vertices(vertices.iter().cloned(), indices.iter().cloned());
+        window.present()
+    }
+}
+
+fn main() {
+    run::<RgbTriangle>(WindowBuilder::new("RGB Triangle", 800, 600)).unwrap();
+}
