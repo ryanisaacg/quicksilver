efe3548a165edbc7cb827431faafc1e31ffb65ca
Kevin Becker <k.becker@btr.zone>
2018-08-22 03:50:30 +0200

    correct stopwatch example and fix line `with_thickness()` method

--- examples/stopwatch.rs
+++ examples/stopwatch.rs
@@ -3,7 +3,7 @@ extern crate quicksilver;
 
 use quicksilver::{
     Result,
-    geom::{Circle, Line, Rectangle, Transform, Triangle, Vector},
+    geom::{Circle, Line, Vector},
     graphics::{Background::Col, Color},
     lifecycle::{Settings, State, Window, run},
 };
@@ -22,20 +22,16 @@ impl State for Stopwatch {
         Ok(Stopwatch {elapsed: 0., hours: 0., minutes: 0., seconds: 0.})
     }
 
-    fn update(&mut self, window: &mut Window, delta_time: f64) -> Result<()> {
+    fn update(&mut self, _window: &mut Window, delta_time: f64) -> Result<()> {
         //println!("Last Update: {:.10?} ms ago", delta_time);
         self.elapsed += delta_time;
-        let seconds = (self.elapsed / 1000.) % 60.;
-        let minutes = ((self.elapsed / 1000.) / 60.) % 60.;
-        let hours = ((self.elapsed / 1000.) / 60. / 24.) % 24.;
-
-        self.seconds = seconds;
-        self.minutes = minutes;
-        self.hours = hours;
+        self.seconds = (self.elapsed / 1000.) % 60.;
+        self.minutes = ((self.elapsed / 1000.) / 60.) % 60.;
+        self.hours = ((self.elapsed / 1000.) / 60. / 24.) % 24.;
         Ok(())
     }
 
-    fn draw(&mut self, window: &mut Window, delta_time: f64) -> Result<()> {
+    fn draw(&mut self, window: &mut Window, _delta_time: f64) -> Result<()> {
         //println!("Last Draw: {:.10?} ms ago", delta_time);
 
         // clear everything
@@ -50,31 +46,24 @@ impl State for Stopwatch {
             let angle = 2. * PI * ((i as f64 + 9.) * 2. / 24.);
             let pos_x = 400. + 200. * angle.cos() as f32;
             let pos_y = 300. + 200. * angle.sin() as f32;
-            let line = Line::new((400, 300), (pos_x, pos_y)).with_thickness(5.);
+            let line = Line::new((400, 300), (pos_x, pos_y)).with_thickness(5);
             window.draw(&line, Col(Color::BLACK));
         }
 
         window.draw(&Circle::new((400, 300), 180), Col(Color::WHITE));
 
 
-        let hour_angle = 2. * PI * ((self.hours+9.) * 2. / 24.);
-        let minute_angle = 2. * PI * ((self.minutes+45.) / 60.);
-        let second_angle = 2. * PI * ((self.seconds+45.) / 60.);
+        let hour_angle = 360. * ((self.hours+9.) * 2. / 24.);
+        let minute_angle = 360. * ((self.minutes+45.) / 60.);
+        let second_angle = 360. * ((self.seconds+45.) / 60.);
 
-        let hour_pos_x = 400. + 150. * hour_angle.cos() as f32;
-        let hour_pos_y = 300. + 150. * hour_angle.sin() as f32;
+        let hour_pos = Vector::from_angle(hour_angle as f32) * 150. + Vector::new(400, 300);
+        let min_pos = Vector::from_angle(minute_angle as f32) * 180. + Vector::new(400, 300);
+        let second_pos = Vector::from_angle(second_angle as f32) * 180. + Vector::new(400, 300);
 
-        let min_pos_x = 400. + 180. * minute_angle.cos() as f32;
-        let min_pos_y = 300. + 180. * minute_angle.sin() as f32;
-
-        let sec_pos_x = 400. + 180. * second_angle.cos() as f32;
-        let sec_pos_y = 300. + 180. * second_angle.sin() as f32;
-
-
-
-        let hour = Line::new((400, 300), (hour_pos_x, hour_pos_y)).with_thickness(10.);
-        let minute = Line::new((400, 300), (min_pos_x, min_pos_y)).with_thickness(5.);
-        let second = Line::new((400, 300), (sec_pos_x, sec_pos_y)).with_thickness(3.);
+        let hour = Line::new((400, 300), hour_pos).with_thickness(10);
+        let minute = Line::new((400, 300), min_pos).with_thickness(5);
+        let second = Line::new((400, 300), second_pos).with_thickness(3);
 
         window.draw(&hour, Col(Color::BLACK));
         window.draw(&minute, Col(Color::BLUE));
@@ -85,10 +74,10 @@ impl State for Stopwatch {
 }
 
 fn main() {
-    let mut settings = Settings::default();
-    settings.draw_rate = 1000. / 60.; // 60 fps
-    settings.update_rate = 1.;
-
-    run::<Stopwatch>("Stopwatch", Vector::new(800, 600), settings);
+    run::<Stopwatch>("Stopwatch", Vector::new(800, 600), Settings {
+        draw_rate: 1000. / 60., // 60 fps
+        update_rate: 1., // every ms to make it appear "smooth"
+        ..Settings::default()
+    });
 }
 
