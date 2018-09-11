a5912cac3f1e502948c942f3bfd0f3a2e7de8374
Ryan Goldstein <ryanisaacg@gmail.com>
2018-09-11 09:41:25 -0400

    Clean up the tutorials to bring them in line with the final plan

--- src/tutorials/mod.rs
+++ src/tutorials/mod.rs
@@ -5,7 +5,7 @@
 //!
 //! Before you jump into the tutorials below, make sure your development environment is ready. If
 //! you're just targeting desktop, all you need is the latest stable Rust. If you're targeting the
-//! web, first make sure you have a nightly toolchain installed (`rustup update nightly`), and the 
+//! web, first make sure you have a nightly toolchain installed (`rustup update nightly`), and the
 //! wasm target installed on nightly (`rustup target add wasm32-unknown-unknown --toolchain
 //! nightly`.) Once that's done, install cargo-web (`cargo +nightly install -f cargo-web`) and you
 //! should be good.
@@ -16,13 +16,11 @@ pub mod _03_input;
 pub mod _04_lifecycle;
 pub mod _05_images;
 pub mod _06_asset_combinators;
-/*pub mod _07_font;
+pub mod _07_font;
 pub mod _08_sound;
-pub mod _09_animations;
+pub mod _09_animations_and_atlases;
 pub mod _10_view;
-pub mod _11_multi_screen;
-pub mod _12_mesh;
-pub mod _13_ncollide_integration;
-pub mod _14_lyon_integration;
-pub mod _15_futures_integration;
+/*
+pub mod _11_mesh;
+pub mod _12_integrations;
 */
