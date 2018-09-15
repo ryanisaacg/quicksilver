0e841ff02e0d3cb1d30d388874f3f47458331535
Ryan Goldstein <ryanisaacg@gmail.com>
2018-09-15 16:32:34 -0400

    Create a script that automates most of the release procedures

---
+++ create-release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+cargo test
+for example in examples/*
+do
+    example=$(basename $example .rs)
+    echo "---- Building example: $example ----"
+    cargo +nightly web build --target wasm32-unknown-unknown --release --example $example
+    echo "--- Copying build artifacts: $example ----"
+    cp target/wasm32-unknown-unknown/release/examples/$example.wasm docs/wasm/$example.wasm
+    cp target/wasm32-unknown-unknown/release/examples/$example.js docs/wasm/$example.js
+done
+
+echo "--- Copying assets to the web directory ---"
+cp static/* docs/
+read -p "New version string: " version
+
+echo "--- Bumping version of the html_url_root ---"
+sed -e "s/html_root_url = \".*\"/html_root_url = \"https://docs.rs\/quicksilver\/$version\/quicksilver\"/g" -i src/lib.rs
+
+echo "--- Bumping version in Cargo.toml ---"
+sed -e "s/^version = \".*\"/version=\"$version\"/" -i Cargo.toml
+
