e040f6c3f8ca1c0148eaf244a958940fad3ded03
Ryan Goldstein <ryanisaacg@gmail.com>
2018-12-12 10:27:37 -0500

    Show more features of immi in the example

--- examples/immi.rs
+++ examples/immi.rs
@@ -1,43 +1,66 @@
 use quicksilver::{
-    Result,
-    Future,
+    Result, Future,
+    combinators::result,
     geom::Vector,
-    graphics::{Color, create_immi_ctx, ImmiStatus, ImmiRender, Font, Image},
+    graphics::{Color, create_immi_ctx, ImmiStatus, ImmiRender, Font, FontStyle, Image},
     lifecycle::{Settings, State, Window, Asset, run},
 };
-use immi::{widgets::image, Alignment};
+use immi::{
+    Alignment,
+    widgets::{Interaction, image_button}
+};
 
-#[derive(Clone)]
-struct UiState {
-    immi_state: immi::UiState,
+struct ImmiExample {
+    ui_state: immi::UiState,
+    assets: Asset<(Font, ButtonState)>,
 }
 
-struct ImmiTest {
-    ui_state: UiState,
-    assets: Asset<(Font, Image)>,
+struct ButtonState {
+    normal: Image,
+    hovered: Image,
+    active: Image,
 }
 
-impl State for ImmiTest {
-    fn new() -> Result<ImmiTest> {
-        Ok(ImmiTest {
-            ui_state: UiState {
-                immi_state: Default::default(),
-            },
+impl ButtonState {
+    fn new(font: Font, style: &FontStyle) -> Result<(Font, ButtonState)> {
+        let normal = font.render("Normal Button", &style)?;
+        let hovered = font.render("Hovered Button", &style)?;
+        let active = font.render("Active Button", &style)?;
+        Ok((font, ButtonState { normal, hovered, active }))
+    }
+}
+
+impl State for ImmiExample {
+    fn new() -> Result<ImmiExample> {
+        Ok(ImmiExample {
+            ui_state: Default::default(),
+            // Load the font and draw the text for the 3 button states
             assets: Asset::new(
-                Font::load("font.ttf").join(
-                    Image::load("image.png"))),
+                Font::load("font.ttf").and_then(|font| {
+                    let style = FontStyle::new(48.0, Color::BLACK);
+                    result(ButtonState::new(font, &style))
+                }))
         })
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
-        window.clear(Color::BLACK)?;
+        window.clear(Color::WHITE)?;
 
-        let ui_state = self.ui_state.clone();
-        self.assets.execute(|(font, image)| {
+        let ui_state = &mut self.ui_state;
+        self.assets.execute(|(font, button)| {
+            // Set up IMMI resources
             let ui_status = ImmiStatus::new(window);
             let mut ui_render = ImmiRender::new_with_window(window, font);
-            let ui_context = create_immi_ctx(ui_status, &mut ui_render);
-            image::draw(&ui_context, image, &Alignment::center());
+            let ui_context = create_immi_ctx(ui_status, &mut ui_render)
+                // Only take up half the screen with the immi widgets
+                .rescale(0.5, 0.5, &Alignment::center());
+
+            // Draw a button widget and if it's clicked, print test
+            match image_button::draw(&ui_context, ui_state, &button.normal, &button.hovered, &button.active, &Alignment::center()) {
+                Interaction::Clicked => println!("Test!"),
+                _ => ()
+            }
+
             Ok(())
         })?;
         Ok(())
@@ -45,14 +68,5 @@ impl State for ImmiTest {
 }
 
 pub fn main() {
-    let mut settings = Settings::default();
-    settings.fullscreen = true;
-    run::<ImmiTest>(
-        "ImmiTest",
-        Vector::new(1680.0, 1050.0),
-        Settings {
-            fullscreen: true,
-            ..Settings::default()
-        },
-    );
+    run::<ImmiExample>("Immi Example", Vector::new(800, 600), Settings::default());
 }
