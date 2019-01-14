b3b6e86b29bb31cb40bf5308ec863e56a5b5f9c4
Ryan Goldstein <ryanisaacg@gmail.com>
2019-01-14 16:23:33 -0500

    Remove unused provider module

--- src/sound/provider.rs
+++
@@ -1,8 +0,0 @@
-pub trait AudioProvider {
-    fn new() -> Self where Self: Sized;
-    fn play(&mut self, data: SoundData, volume);
-    fn get_generation(&self) -> u32;
-    fn play(&mut self);
-    fn pause(&mut self);
-    fn is_paused(&self) -> bool;
-}
