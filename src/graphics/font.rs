c98033927cada4ac1cc7d17b2ebe8fe446103bd2
Tomas Sedovic <tomas@sedovic.cz>
2019-02-03 13:42:04 +0100

    Add multiline text rendering
    This extends the `Font::render` method to handle string slices
    containing line breaks (`\n`).
    
    Fixes #457

--- src/graphics/font.rs
+++ src/graphics/font.rs
@@ -37,40 +37,56 @@ impl Font {
 
     /// Render a text string to an Image
     ///
-    /// This function does not take into account unicode normalization or vertical layout
+    /// This function handles line breaks but it does not take into account unicode
+    /// normalization or other text formatting.
     pub fn render(&self, text: &str, style: &FontStyle) -> Result<Image> {
         let scale = Scale { x: style.size, y: style.size };
-        //Avoid clipping
-        let offset = point(0.0, self.data.v_metrics(scale).ascent);
-        let glyphs = self.data.layout(text, scale, offset).collect::<Vec<PositionedGlyph>>();
-        let width = glyphs.iter().rev()
-            .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
-            .next().unwrap_or(0.0).ceil() as usize;
-        let mut pixels = vec![0 as u8; 4 * width * style.size as usize];
-        for glyph in glyphs {
-            if let Some(bounds) = glyph.pixel_bounding_box() {
-                glyph.draw(|x, y, v| {
-                    // `bounds.min` can contain negative numbers:
-                    let bound_min_x = std::cmp::max(0, bounds.min.x) as u32;
-                    let bound_min_y = std::cmp::max(0, bounds.min.y) as u32;
-                    let x = x + bound_min_x;
-                    let y = y + bound_min_y;
-                    // x or y can be greater than our pixels area:
-                    if x < width as u32 && y < style.size as u32 {
-                        let index = (4 * (x + y * width as u32)) as usize;
-                        let red = (255.0 * style.color.r) as u8;
-                        let green = (255.0 * style.color.g) as u8;
-                        let blue = (255.0 * style.color.b) as u8;
-                        let alpha = (255.0 * v) as u8;
-                        let bytes = [red, green, blue, alpha];
-                        for i in 0..bytes.len() {
-                            pixels[index + i] = bytes[i];
+        let line_count = text.lines().count();
+        let glyphs_per_line = text
+            .lines()
+            .map(|text| {
+                //Avoid clipping
+                let offset = point(0.0, self.data.v_metrics(scale).ascent);
+                let glyphs = self.data.layout(text.trim_end(), scale, offset)
+                    .collect::<Vec<PositionedGlyph>>();
+                let width = glyphs.iter().rev()
+                    .map(|g|
+                        g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+                    .next().unwrap_or(0.0).ceil() as usize;
+                (glyphs, width)
+            })
+            .collect::<Vec<_>>();
+        let max_width = *glyphs_per_line.iter().map(|(_, width)| width).max().unwrap_or(&0);
+        let mut pixels = vec![0 as u8; 4 * line_count * max_width * style.size as usize];
+        for (line_index, (glyphs, width)) in glyphs_per_line.iter().enumerate() {
+            let width = *width;
+            let line_offset = line_index * 4 * max_width * style.size as usize;
+            for glyph in glyphs {
+                if let Some(bounds) = glyph.pixel_bounding_box() {
+                    glyph.draw(|x, y, v| {
+                        // `bounds.min` can contain negative numbers:
+                        let bound_min_x = std::cmp::max(0, bounds.min.x) as u32;
+                        let bound_min_y = std::cmp::max(0, bounds.min.y) as u32;
+                        let x = x + bound_min_x;
+                        let y = y + bound_min_y;
+                        // x or y can be greater than our pixels area:
+                        if x < width as u32 && y < style.size as u32 {
+                            let index = line_offset + (4 * (x + y * max_width as u32)) as usize;
+                            let red = (255.0 * style.color.r) as u8;
+                            let green = (255.0 * style.color.g) as u8;
+                            let blue = (255.0 * style.color.b) as u8;
+                            let alpha = (255.0 * v) as u8;
+                            let bytes = [red, green, blue, alpha];
+                            for i in 0..bytes.len() {
+                                pixels[index + i] = bytes[i];
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         }
-        Image::from_raw(pixels.as_slice(), width as u32, style.size as u32, PixelFormat::RGBA)
+        Image::from_raw(pixels.as_slice(), max_width as u32,
+                        line_count as u32 * style.size as u32, PixelFormat::RGBA)
     }
 }
 
