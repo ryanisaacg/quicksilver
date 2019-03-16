de4db6508ea4aa42d17ea6f9f09894151eda7f41
Ryan Goldstein <ryanisaacg@gmail.com>
2019-03-16 18:25:56 -0400

    Remove merge artifacts

--- examples/lyon.rs
+++ examples/lyon.rs
@@ -9,15 +9,7 @@ use quicksilver::{
     lyon::{
         extra::rust_logo::build_logo_path,
         tessellation::{FillTessellator, FillOptions},
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         path::{Path, builder::*},
-=======
-        path::{builder::*, Path}
->>>>>>> Stashed changes
-=======
-        path::{builder::*, Path}
->>>>>>> Stashed changes
     }
 };
 
