f47dfa9cb50aa70a3142df4e06caa4150081de2e
Ryan G <ryanisaacg@users.noreply.github.com>
2019-03-17 08:49:00 -0400

    Create a prelude module (#479)
    * Create a prelude module
    
    * Update CHANGELOG

---
+++ src/prelude.rs
@@ -0,0 +1,17 @@
+//! A collection of common imports for convenience
+
+pub use crate::{
+    Error, Future, Result, load_file,
+    combinators::{join_all, result},
+    geom::*,
+    graphics::{Background::{self, *}, Color, Image},
+    input::{ButtonState, Key, MouseButton},
+    lifecycle::{Asset, Event, Settings, State, Window, run}
+};
+#[cfg(feature = "fonts")]
+pub use crate::graphics::{Font, FontStyle};
+#[cfg(feature = "gamepads")]
+pub use crate::input::{GamepadAxis, GamepadButton};
+#[cfg(feature = "sounds")]
+pub use crate::sound::Sound;
+
