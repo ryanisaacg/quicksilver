3ea6f90323e27950d1649fedf4652b5a5b28077e
Dallas <dallascoble@outlook.com>
2019-07-19 12:38:21 +0800

    Add `transform_bounding_box` to Shape (#515)
    Transform a shape then get a bounding box for the transformed shape

--- src/geom/shape.rs
+++ src/geom/shape.rs
@@ -1,4 +1,4 @@
-use crate::geom::{Circle, Line, Rectangle, Triangle, Vector, about_equal};
+use crate::geom::{Circle, Line, Rectangle, Triangle, Vector, Transform, about_equal};
 
 /// The collision and positional attributes of shapes
 pub trait Shape {
@@ -24,6 +24,28 @@ pub trait Shape {
     /// A Rectangle that contains the entire shape
     #[must_use]
     fn bounding_box(&self) -> Rectangle;
+    /// Apply a transform to a shape then get the bounding box for the transformed shape
+    ///
+    /// Note: if you want to get the collision of rotated shapes, you probably
+    /// want to use [ncollide](https://crates.io/crates/ncollide)
+    #[must_use]
+    fn transformed_bounding_box(&self, transform: Transform) -> Rectangle {
+        let bb = self.bounding_box();
+        // Build the transform to rotate the shape
+        let transform = Transform::translate(bb.center() + bb.pos)
+            * transform
+            * Transform::translate(-bb.pos - bb.center());
+        // Get new corner position
+        let tl = transform * bb.pos;
+        let tr = transform * (bb.pos + Vector::new(bb.size.x, 0));
+        let bl = transform * (bb.pos + Vector::new(0, bb.size.y));
+        let br = transform * (bb.pos + bb.size);
+        // Get min and max points
+        let min = tr.min(tl).min(bl).min(br);
+        let max = tr.max(tl).max(bl).max(br);
+        // Make new bounding box
+        Rectangle::new(min, max - min)
+    }
     /// Create a copy of the shape with an offset center
     #[must_use]
     fn translate(&self, amount: impl Into<Vector>) -> Self where Self: Sized;
