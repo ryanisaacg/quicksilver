9f6480bad7fd0207f1146253c4a932cce884d9c3
Ryan Goldstein <ryan@ryanisaacg.com>
2020-01-10 13:11:47 -0500

    Document the new APIs and the changes

--- src/graphics.rs
+++ src/graphics.rs
@@ -518,10 +518,12 @@ impl Image {
         Image::from_encoded_bytes(gfx, file_contents.as_slice())
     }
 
+    /// Replace the backing data for the image, or create a blank image
     pub fn set_data(&mut self, data: Option<&[u8]>, width: u32, height: u32, color: ColorFormat) {
         self.0.borrow_mut().set_image(data, width, height, color);
     }
 
+    /// Set the data for some region of this image, without clearing it
     pub fn set_sub_data(
         &self,
         data: &[u8],
@@ -575,9 +577,20 @@ impl Image {
     }
 }
 
+/// A Surface is the core struct for rendering to textures, or getting data from them.
+///
+/// If you want to render to a texture, [`attach`] it and then pass the surface to
+/// [`Graphics::flush`].
+/// 
+/// If you want to get data from a texture, [`attach`] it and use [`Surface::screenshot`].
+///
+/// [`attach`]: Surface::attach
 pub struct Surface(golem::Surface);
 
 impl Surface {
+    /// Create a Surface with an attached Image
+    ///
+    /// The image must not have any other references to it, or this function will return an error.
     pub fn new(gfx: &Graphics, attachment: Image) -> Result<Surface, QuicksilverError> {
         let tex = attachment
             .into_raw()
@@ -585,6 +598,17 @@ impl Surface {
         Ok(Surface(golem::Surface::new(&gfx.ctx, tex)?))
     }
 
+    /// Use the attached image as the backing data for this Surface
+    ///
+    /// To either get the data for an image via [`Surface::screenshot`] or set it via
+    /// [`Graphics::flush`], an image needs to be attached to this Surface.
+    ///
+    /// The image must not have any other references to it, or this function will return an error.
+    ///
+    /// It's generally faster to create one [`Surface`] per [`Image`], and only attach and
+    /// [`detach`] when necessary.
+    ///
+    /// [`detach`]: Surface::detach
     pub fn attach(&mut self, attachment: Image) -> Result<(), QuicksilverError> {
         let tex = attachment
             .into_raw()
@@ -593,9 +617,31 @@ impl Surface {
 
         Ok(())
     }
-
+    
+    /// Take the Image out of this Surface
+    ///
+    /// To use the data that has been rendered to a Surface, its attachment has to be removed to
+    /// avoid creating a loop (where the Image is both being drawn *from* and being drawn *to*.)
     pub fn detach(&mut self) -> Option<Image> {
         Some(Image::new(self.0.take_texture()?))
+    }
+
+    /// Get the pixel data of a given region of this surface
+    pub fn screenshot(
+        &self,
+        gfx: &Graphics,
+        x: u32,
+        y: u32,
+        width: u32,
+        height: u32,
+        format: ColorFormat,
+    ) -> Vec<u8> {
+        self.0.bind();
+        let mut buffer = vec![0; (width * height * format.bytes_per_pixel()) as usize];
+        self.0.get_pixel_data(x, y, width, height, format, &mut buffer[..]);
+        golem::Surface::unbind(&gfx.ctx);
+
+        buffer
     }
 }
 
