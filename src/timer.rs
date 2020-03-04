dff137fc409068a6654827b262bc90ef90fb4386
lenscas <lenscas@gmail.com>
2020-03-04 19:44:48 +0100

    cargo fmt

--- src/timer.rs
+++ src/timer.rs
@@ -2,29 +2,29 @@ use instant::Instant;
 use std::time::Duration;
 
 pub struct Timer {
-	period: Duration,
-	init: Instant,
+    period: Duration,
+    init: Instant,
 }
 
 impl Timer {
-	pub fn time_per_second(times: f32) -> Timer {
-		Timer::with_duration(Duration::from_secs_f32(1.0 / times))
-	}
-	pub fn with_duration(period: Duration) -> Timer {
-		Timer {
-			period,
-			init: Instant::now(),
-		}
-	}
-	pub fn tick(&mut self) -> bool {
-		if self.init.elapsed() >= self.period {
-			self.init = Instant::now();
-			true
-		} else {
-			false
-		}
-	}
-	pub fn remaining(&self) -> Option<Duration> {
-		self.init.elapsed().checked_sub(self.period)
-	}
+    pub fn time_per_second(times: f32) -> Timer {
+        Timer::with_duration(Duration::from_secs_f32(1.0 / times))
+    }
+    pub fn with_duration(period: Duration) -> Timer {
+        Timer {
+            period,
+            init: Instant::now(),
+        }
+    }
+    pub fn tick(&mut self) -> bool {
+        if self.init.elapsed() >= self.period {
+            self.init = Instant::now();
+            true
+        } else {
+            false
+        }
+    }
+    pub fn remaining(&self) -> Option<Duration> {
+        self.init.elapsed().checked_sub(self.period)
+    }
 }
