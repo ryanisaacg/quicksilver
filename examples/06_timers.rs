e22edb126e75a7fb072ff30318dc1f7507242111
lenscas <lenscas@gmail.com>
2020-03-04 22:45:22 +0100

    Add documentation

---
+++ examples/06_timers.rs
@@ -0,0 +1,46 @@
+// Example 6: The Window
+// Use timers to know when to draw and to have a consistent update cycle.
+use quicksilver::{
+    geom::{Rectangle, Vector},
+    graphics::{Color, Graphics},
+    lifecycle::{run, EventStream, Settings, Window},
+    Result, Timer,
+};
+
+fn main() {
+    run(
+        Settings {
+            size: Vector::new(800.0, 600.0).into(),
+            title: "Square Example",
+            ..Settings::default()
+        },
+        app,
+    );
+}
+
+async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Result<()> {
+    // Clear the screen to a blank, white color
+    gfx.clear(Color::WHITE);
+
+    //Here we make 2 kinds of timers.
+    //One to provide an consistant update time, so our example updates 30 times per second
+    //the other informs us when to draw the next frame, this causes our example to draw 60 times per second
+    let mut update_timer = Timer::time_per_second(30.0);
+    let mut draw_timer = Timer::time_per_second(60.0);
+
+    let mut rect = Rectangle::new(Vector::new(0.0, 100.0), Vector::new(100.0, 100.0));
+
+    loop {
+        while let Some(_) = events.next_event().await {}
+        if update_timer.tick() {
+            rect.pos.x += 5.0;
+        }
+
+        if draw_timer.tick() {
+            gfx.clear(Color::WHITE);
+            gfx.fill_rect(&rect, Color::BLUE);
+            gfx.stroke_rect(&rect, Color::RED);
+            gfx.present(&window)?;
+        }
+    }
+}
--- src/timer.rs
+++ src/timer.rs
@@ -1,6 +1,6 @@
 use instant::Instant;
 use std::time::Duration;
-
+///A timer that you can use to fix the time between actions, for example updates or draw calls.
 pub struct Timer {
     period: Duration,
     init: Instant,
@@ -10,12 +10,15 @@ impl Timer {
     pub fn time_per_second(times: f32) -> Timer {
         Timer::with_duration(Duration::from_secs_f32(1.0 / times))
     }
+
     pub fn with_duration(period: Duration) -> Timer {
         Timer {
             period,
             init: Instant::now(),
         }
     }
+
+    ///Look if the time has elapsed and if so, starts the countdown for the next tick.
     pub fn tick(&mut self) -> bool {
         if self.init.elapsed() >= self.period {
             self.init = Instant::now();
@@ -24,6 +27,8 @@ impl Timer {
             false
         }
     }
+
+    ///look how much time is still left before its time for next tick.
     pub fn remaining(&self) -> Option<Duration> {
         self.init.elapsed().checked_sub(self.period)
     }
