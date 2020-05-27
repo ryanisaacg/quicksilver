c4a1d69d51d7fd5078fb7ef4f7d34e51566a18bf
Ryan Goldstein <ryan@ryanisaacg.com>
2020-05-26 20:54:58 -0400

    Bring the example up to the latest master

--- examples/07_resize.rs
+++ examples/07_resize.rs
@@ -1,16 +1,21 @@
 // Example 7: Resize Handling
 // Show the different ways of resizing the window
 use quicksilver::{
-    geom::{Rectangle, Vector},
+    geom::{Rectangle, Transform, Vector},
     graphics::{Color, Element, Graphics, Mesh, ResizeHandler, Vertex},
-    lifecycle::{run, Event, EventStream, Settings, Window},
-    Result,
+    input::Event,
+    Input, Window, Result, Settings, run,
+};
+
+const SIZE: Vector = Vector {
+    x: 800.0,
+    y: 600.0,
 };
 
 fn main() {
     run(
         Settings {
-            size: Vector::new(800.0, 600.0).into(),
+            size: SIZE,
             title: "RGB Triangle Example",
             resizable: true,
             ..Settings::default()
@@ -19,22 +24,22 @@ fn main() {
     );
 }
 
-async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Result<()> {
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
     // Paint a triangle with red, green, and blue vertices, blending the colors for the pixels in-between
     // Define the 3 vertices and move them inside a Vec
     let vertices = {
         let top = Vertex {
-            pos: Vector::new(400, 200),
+            pos: Vector::new(400.0, 200.0),
             uv: None,
             color: Color::RED,
         };
         let left = Vertex {
-            pos: Vector::new(200, 400),
+            pos: Vector::new(200.0, 400.0),
             uv: None,
             color: Color::GREEN,
         };
         let right = Vertex {
-            pos: Vector::new(600, 400),
+            pos: Vector::new(600.0, 400.0),
             uv: None,
             color: Color::BLUE,
         };
@@ -52,16 +57,18 @@ async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Resu
         aspect_width: 4.0,
         aspect_height: 3.0,
     };
+    let projection = Transform::orthographic(Rectangle::new_sized(SIZE));
     loop {
-        while let Some(ev) = events.next_event().await {
+        while let Some(ev) = input.next_event().await {
             if let Event::Resized(ev) = ev {
-                gfx.set_projection(resize_handler.projection(ev.logical_size()));
+                let letterbox = resize_handler.projection(ev.size());
+                gfx.set_projection(letterbox * projection);
             }
         }
         // Clear the screen to a blank, black color
         gfx.clear(Color::BLACK);
         // Fill the relevant part of the screen with white
-        let rect = Rectangle::new_sized(Vector::new(800.0, 600.0));
+        let rect = Rectangle::new_sized(SIZE);
         gfx.fill_rect(&rect, Color::WHITE);
         // Pass a reference to the mesh to the graphics object to draw
         gfx.draw_mesh(&mesh);
